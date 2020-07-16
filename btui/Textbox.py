5e0e6efcbffef7710a832fac56b9b15d5544c1d3
Astelon <lg.astelon@astelon.dev>
2020-07-15 18:28:55 -0700

    Add test case for Textbox and edit and focus styles

--- btui/Textbox.py
+++ btui/Textbox.py
@@ -7,16 +7,17 @@ class Textbox :
         - width       (int)     width of the box
         - height      (int)     height of the box
         - text        (string)  the text in the box
-        - style       (string)  the FG color 
-        - focus_style (string)
+        - style       (string)  the basic style to be used when not edited or focused
+        - edit_style  (string)  The style to be used on edit mode
+        - focus_style (string)  The style to be used on focus
         - border      (bool)    true/false
         - multiline   (bool)    Whether the textbox can accept many lines or not
-        - 
     """
     attributes = {}
     cursorX = 0
     cursorY = 0
-    focused = False
+    focused  = False
+    editting = False
 
     def __init__(self, attr):
         self.attributes = attr
@@ -36,18 +37,55 @@ class Textbox :
         self.cursorY += 1
 
     def draw(self,term) :
+        x = self.attributes.get('x',0)
+        y = self.attributes.get('y',0)
         text = self.attributes.get('text')
-        print(term.move_xy(self.attributes.get('x'), self.attributes.get('y')))
-
+        print(term.move_xy(x, y))
         if self.focused:
             print(self.attributes.get('focus_style', self.attributes.get('style', '')))
         else:
             print(self.attributes.get('style', self.attributes.get('focus_style', '')))
 
-        text + = ' ' * (self.attributes.get('width') - len(text))
+        if self.editting:
+            print(self.attributes.get('edit_style', self.attributes.get('focus_style', '')))
+
+        text += ' ' * (self.attributes.get('width') - len(text))
         print(text)
-        print(term.move_xy(self.cursorX, self.cursorY))
+        if self.focused or self.editting:
+            print(term.move_xy(self.cursorX + x, self.cursorY + y))
+
+    def text(self):
+        return self.attributes.get('text','')
+
+    def setFocused(self, focus):
+        self.focused = focus
+
+    def edit(self, term):
+        self.editting = True
+
+        self.editting = False
+
 
 if __name__ == '__main__':
     term = Terminal()
-    tb = Textbox({  })
+    result = ''
+    with term.fullscreen(), term.cbreak():
+        tb = Textbox({
+                'x'     : 15,
+                'y'     : 5,
+                'width' : 30,
+                'style' : term.black_on_white,
+                'edit_style'  : term.underline + term.black_on_lightgreen,
+                'focus_style' : term.underline + term.black_on_white,
+                'text'  : 'Hello Box'
+                })
+
+        tb.draw(term)
+        term.inkey()
+
+        ############### TEARDOWN ################
+        print(term.on_black)
+        result = tb.text()
+
+    print(f'Text read: {result}')
+
